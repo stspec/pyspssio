4b4b132263ce3373c3439af306d7f586796238bf
stspec <110640641+stspec@users.noreply.github.com>
2024-05-12 23:07:17 -0700

    Improve cli functionality
     - convert cli inputs to correct types
     - improve help/error messages
     - add data_only parameter to read_sav user function

--- pyspssio/command_line.py
+++ pyspssio/command_line.py
@@ -14,34 +14,72 @@
 # =============================================================================
 
 import sys
+import ast
+import json
 import argparse
-from inspect import signature, getmembers, isfunction
+from inspect import signature, getmembers, isfunction, Parameter
+from pandas import DataFrame
 
 from . import user_functions
 
 
+def boolean(value):
+    """Safely convert input value to boolean"""
+    if isinstance(value, str):
+        value = value.title()
+    return bool(ast.literal_eval(value))
+
+
+def dictionary(value):
+    """Safely convert input value to dictionary"""
+    if isinstance(value, str):
+        value = json.loads(value)
+    return dict(value)
+
+
+def dataframe(value):
+    """Safely convert input value to pandas DataFrame"""
+    if isinstance(value, str):
+        try:
+            value = json.loads(value)
+        except Exception:
+            try:
+                value = ast.literal_eval(value.title())
+            except Exception:
+                pass
+
+    return DataFrame(value)
+
+
 funcs = {}
 for func_name, func in getmembers(user_functions, isfunction):
-    parser = argparse.ArgumentParser()
-    func_help = ""
-    for name, parameter in signature(func).parameters.items():
-        parser.add_argument("--" + name)
-        func_help += "\n - " + str(parameter)
+    parser = argparse.ArgumentParser(formatter_class=argparse.MetavarTypeHelpFormatter)
+    parameters = signature(func).parameters
+    for name, parameter in parameters.items():
+        default = None if parameter.default == Parameter.empty else parameter.default
+        if parameter.annotation in [str, int, float]:
+            parser.add_argument("--" + name, type=parameter.annotation, default=default)
+        elif parameter.annotation == bool:
+            parser.add_argument("--" + name, type=boolean, default=default)
+        elif parameter.annotation == dict:
+            parser.add_argument("--" + name, type=dictionary, default=default)
+        elif name == "df":
+            parser.add_argument("--" + name, type=dataframe)
+        elif name == "usecols":
+            parser.add_argument("--" + name, type=str, nargs="*")
+        elif name == "kwargs":
+            continue
+        else:
+            parser.add_argument("--" + name, type=str, default=default)
 
-    funcs[func_name] = {"func": func, "parser": parser, "help": func_help.strip("\n")}
+    funcs[func_name] = {"func": func, "parser": parser, "help": parser.format_help()}
 
 help_message = f'''
-### Usage ###
-
+Usage:
 pyspssio <function> --arg1 arg1 --arg2 arg2
 
-### Functions ###
-
-{'\n'.join(' - ' + func_name for func_name in funcs.keys())}
-
-### Function Signatures ###
-
-{'\n\n'.join(name + '\n' + func['help'] for name, func in funcs.items())}
+Functions:
+{'\n'.join(' - ' + func_name for func_name in funcs)}
 
 '''
 
@@ -49,7 +87,12 @@ pyspssio <function> --arg1 arg1 --arg2 arg2
 def call_function(func_name):
     """Helper function for calling function"""
 
-    func_info = funcs[func_name]
+    try:
+        func_info = funcs[func_name]
+    except KeyError:
+        sys.stderr.write(f"{help_message}\nFunction not recognized: {func_name}\n")
+        return None
+
     parser = func_info["parser"]
     func = func_info["func"]
     kwargs = vars(parser.parse_args())
@@ -57,7 +100,8 @@ def call_function(func_name):
     try:
         return func(**kwargs)
     except Exception as err:
-        print(err, kwargs, func_info["help"], sep="\n\n")
+        sys.stderr.write(f'\n{func_info["help"]}\n\n{kwargs}\n\n{err}\n')
+        return None
 
 
 def main():
@@ -73,9 +117,13 @@ def main():
     """
 
     if len(sys.argv) <= 1:
-        print(help_message)
-        return
+        sys.stdout.write(help_message)
+        return None
 
     func_name = sys.argv[1]
+    if func_name.lower() in ["-h", "--help"]:
+        sys.stdout.write(help_message)
+        return None
+
     sys.argv = sys.argv[0:1] + sys.argv[2:]
     return call_function(func_name)
