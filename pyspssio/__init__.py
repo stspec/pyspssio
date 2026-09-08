3ab01042afb8d95286ccd893b66ea508ebcf0c6c
stspec <110640641+stspec@users.noreply.github.com>
2026-09-08 01:23:31 -0700

    Clean up __init__ imports

--- pyspssio/__init__.py
+++ pyspssio/__init__.py
@@ -12,26 +12,18 @@
 # is available in the LICENSE document.
 # =============================================================================
 
-import os
-import platform
-import threading
-import warnings
-
 from . import config
 from .constants import *
 from .errors import SPSSError, SPSSWarning
 from .header import Header
 from .reader import Reader
 from .spssfile import SPSSFile
-from .user_functions import Any as Any
-from .user_functions import DataFrame as DataFrame
-from .user_functions import Generator as Generator
-from .user_functions import Reader as Reader
-from .user_functions import Writer as Writer
-from .user_functions import append_sav as append_sav
-from .user_functions import read_metadata as read_metadata
-from .user_functions import read_sav as read_sav
-from .user_functions import write_sav as write_sav
+from .user_functions import (
+    append_sav,
+    read_metadata,
+    read_sav,
+    write_sav,
+)
 from .writer import Writer
 
 try:
